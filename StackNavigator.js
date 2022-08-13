b016ef0aea44c7bca9d3bb38bef9e24bc3c30445
raouftams <raouftams@gmail.com>
2022-08-13 16:48:59 +0100

    adding home, chat and login screens

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,15 +1,24 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
+import ChatScreen from "./screens/ChatScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
-  return (
-    <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
+    const user = false;
+    return (
+        <Stack.Navigator>
+            {user ? (
+                <>
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                    <Stack.Screen name="Chat" component={ChatScreen} />
+                </>
+            ) : (
+                <Stack.Screen name="Login" component={LoginScreen} />
+            )}
+        </Stack.Navigator>
     
   );
 };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 import { useTailwind } from 'tailwind-rn/dist'
+import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
     const tw = useTailwind()
+    const navigation = useNavigation()
     return (
         <View style={tw("flex-1 justify-center items-center")}>
             <Text style={tw('text-blue-400')}>HomeScreen</Text>
+            <Button title="Go to ChatScreen" onPress={() => navigation.navigate('Chat')} />
         </View>
     )
 }
