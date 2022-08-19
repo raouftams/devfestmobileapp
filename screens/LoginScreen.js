4c6236cbd992169262bc2200ed5150a13185d1e8
raouftams <raouftams@gmail.com>
2022-08-19 15:42:51 +0100

    add login with google button accesing useAuth hook

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import useAuth from '../hooks/useAuth'
 
 const LoginScreen = () => {
-  return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
+    const { signInWithGoogle } = useAuth();
+    return (
+        <View>
+            <Text>LoginScreen</Text>
+            <Button title="Sign in with Google" onPress={signInWithGoogle} />
+        </View>
+    )
 }
 
 export default LoginScreen
