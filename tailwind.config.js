9e158e9e47580509edbd9a90a5e9bb93d36ca89d
raouftams <raouftams@gmail.com>
2022-08-27 18:47:53 +0100

    finishing the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./screens/**/*.{js,jsx,ts,tsx}'],
+  content: ['./screens/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
