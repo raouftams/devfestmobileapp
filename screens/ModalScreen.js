5b744afdc1442568121a632be171a62b94f6a9c2
raouftams <raouftams@gmail.com>
2022-08-27 18:39:50 +0100

    create a model screen for updating new user info

---
+++ screens/ModalScreen.js
@@ -0,0 +1,98 @@
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { useTailwind } from 'tailwind-rn/dist'
+import useAuth from '../hooks/useAuth'
+import { useNavigation } from '@react-navigation/native'
+import { doc, serverTimestamp, setDoc } from 'firebase/firestore'
+import { db } from '../firebase'
+
+const ModalScreen = () => {
+
+    const tw = useTailwind()
+    const navigation = useNavigation()
+    const { user } = useAuth()
+
+    const [image, setImage] = useState(null)
+    const [job, setJob] = useState(null)
+    const [age, setAge] = useState(null)
+
+    const incompleteForm = !image || !job || !age;
+
+
+    const updateUserProfile = () => {
+        setDoc(doc(db, 'users', user.uid), {
+            id: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+            job: job,
+            age: age,
+            timestamp: serverTimestamp()
+        }).then(() => {
+            navigation.navigate("Home")
+        }).catch(error => {
+            alert(error.message);
+        })
+    }
+
+
+    return (
+      <View style={tw("flex-1 items-center pt-3")}>
+        <Image 
+          style={tw("h-20 w-full")}
+          resizeMode="contain"
+          source={{ uri: "https://links.papareact.com/2pf" }}
+        />
+        <Text style={tw("text-xl text-gray-500 p-2 font-bold")}>
+          Welcome {user.displayName}
+        </Text>
+
+          <Text style={tw("text-center p-4 font-bold text-red-400")}>
+            Step 1: The profile pic
+          </Text>
+
+          <TextInput
+              value={image}
+              onChangeText={setImage}
+              style={tw("text-center text-xl pb-2")}
+              placeholder='Enter a profile pic URL'
+          />
+
+          <Text style={tw("text-center p-4 font-bold text-red-400")}>
+              Step 2: The Job
+          </Text>
+
+          <TextInput
+              value={job}
+              onChangeText={setJob}  
+              style={tw("text-center text-xl pb-2")}
+              placeholder='Enter your occupation'
+          />
+
+          <Text style={tw("text-center p-4 font-bold text-red-400")}>
+              Step 3: The age
+          </Text>
+          <TextInput
+              value={age}
+              onChangeText={setAge}  
+              style={tw("text-center text-xl pb-2")}
+              placeholder='Enter your age'
+              keyboardType='numeric'
+              maxLength={2}
+          />
+
+          <TouchableOpacity 
+              disabled={incompleteForm} 
+              style={[
+                  tw("w-64 p-3 rounded-xl absolute bottom-10"),
+                  incompleteForm ? tw("bg-gray-400"): tw("bg-red-400")
+              ]}
+              onPress={updateUserProfile}
+          >
+              <Text style={tw("text-center text-white text-xl")}>Update Profile</Text>
+          </TouchableOpacity>
+            
+      </View>
+    )
+}
+
+export default ModalScreen
