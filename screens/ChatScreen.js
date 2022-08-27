7a7aa44a1aac6527224c1430dc8967edce4c99ec
raouftams <raouftams@gmail.com>
2022-08-27 18:42:33 +0100

    create a chat screen to show all available discussions

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import Header from '../components/Header'
+import ChatList from '../components/ChatList'
 
 const ChatScreen = () => {
   return (
-    <View>
-      <Text>ChatScreen</Text>
-    </View>
+    <SafeAreaView style={{paddingTop: Platform.OS === 'android' ? 15 : 0}}>
+      <Header title="Chat" />
+      <ChatList />
+    </SafeAreaView>
   )
 }
 
